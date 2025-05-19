953a1015b15fb4541c6ea902af79d9a2af252741
Kolomona <kolomona@kolomona.com>
2025-05-19 18:55:05 -0500

    getting ready for testing

--- value-server.js
+++ value-server.js
@@ -1,24 +1,27 @@
 import { createServer } from "http";
 import { Server } from "socket.io";
 
+import dotenv from 'dotenv';
+dotenv.config();
+
 const io = new Server(httpServer, { 
   origin: "*"
  });
 
  let remoteValue = {};
  let extended_data = {
-  "image": "https://assets.godcaster.fm/image_18_1733337133.jpg",
-  "title": "Unknown Title",
+  "image": "https://cdn.kolomona.com/podcasts/lightning-thrashes/000/000-Lightning-Thrashes-Live-1000.jpg",
+  "title": "Lightning Thrashes 24hr Pre-recorded Livestream",
   "line": `line`,
-  "description": "Hello Fred! - 830-326-6365",
+  "description": "This is a test of triggering value time splits for my icecast stream that plays Lightning Thrashes episodes 24/7",
   "value": {},
   "type": "music",
   "link": `link`,
-  "chaptersUrl": `None`,
-  "enclosureUrl": "https://c21.radioboss.fm:8239/stream",
-  "feedGuid": "35f494b2-cf5a-57cf-860e-198676e27d6f",
-  "feedUrl": "https://feeds.godcaster.fm/player_18.xml",
-  "medium": "music",
+  "chaptersUrl": '',
+  "enclosureUrl": process.env.ICECAST_STREAM_URL,
+  "feedGuid": "d5e73072-64a2-56a3-9dcd-4a00bfe561d5",
+  "feedUrl": "https://sirlibre.com/lightning-thrashes-rss.xml",
+  "medium": "podcast",
   "itemGuid": "35f494b2-cf5a-57cf-860e-198676e27d6f1740401883",
   "duration": 333.33,
   "startTime": 0
